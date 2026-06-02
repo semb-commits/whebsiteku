9506fc1cddf0d35e12d3a61c1903b4f95ea540f8
semb-commits <sembsui@gmail.com>
2026-06-02 23:30:16 +0700

    Update background.js

--- background.js
+++ background.js
@@ -10,18 +10,14 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
     const url = new URL(changeInfo.url);
     const hostname = url.hostname;
     
-    // Skip kalau domain resmi
     if (OFFICIAL_DOMAINS.includes(hostname)) return;
     
-    // Cek kemiripan: ada kata "s-developer" tapi domain beda
     if (hostname.includes("s-developer") && !hostname.endsWith("netlify.app")) {
       chrome.action.setBadgeText({tabId, text: "!"});
       chrome.action.setBadgeBackgroundColor({tabId, color: "#ff0000"});
       
-      // Notifikasi
       chrome.notifications.create({
         type: "basic",
-        iconUrl: "icon128.png",
         title: "Peringatan Phishing",
         message: `Situs ${hostname} bukan situs resmi S-Developer. Hati-hati!`
       });
